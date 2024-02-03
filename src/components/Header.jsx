4d512074e9478c66b28dcf370c7056ff7435dba1
grant chichester <gchichester@gmail.com>
2024-02-03 12:15:55 +0700

    created header image

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header () {
+
+  return (
+    <div id="header">
+      <img src="/investment-calculator-logo.png" alt="Investment Calculator Logo" />
+      <h1>Investment Calculator</h1>
+    </div>
+  )
+}
